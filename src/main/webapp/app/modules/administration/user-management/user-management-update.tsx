1aa949716f4aac0407986cd077825fa82008258e
Gia Huy Truong <giahuy300336@gmail.com>
2023-12-28 01:12:31 +0700

    AW-35: Admin - User management

--- src/main/webapp/app/modules/administration/user-management/user-management-update.tsx
+++ src/main/webapp/app/modules/administration/user-management/user-management-update.tsx
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Button, Row, Col, FormText } from 'reactstrap';
+import { Button, Row, Col, FormText, Label } from 'reactstrap';
 import { Translate, translate, ValidatedField, ValidatedForm, isEmail } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { locales, languages } from 'app/config/translation';
 import { getUser, getRoles, updateUser, createUser, reset } from './user-management.reducer';
 import { useAppDispatch, useAppSelector } from 'app/config/store';
+import { MultiSelect } from 'primereact/multiselect';
+import { Checkbox } from 'primereact/checkbox';
+import { Dropdown } from 'primereact/dropdown';
+import { Skeleton } from 'primereact/skeleton';
 
 export const UserManagementUpdate = () => {
   const dispatch = useAppDispatch();
 
   const navigate = useNavigate();
+
+  const [userAuthorities, setUserAuthorities] = useState([]);
+  const [userActivated, setUserActivated] = useState(false);
 
   const { login } = useParams<'login'>();
   const isNew = login === undefined;
@@ -33,6 +40,8 @@ export const UserManagementUpdate = () => {
   };
 
   const saveUser = values => {
+    values.authorities = userAuthorities;
+    values.activated = userActivated;
     if (isNew) {
       dispatch(createUser(values));
     } else {
@@ -47,137 +56,185 @@ export const UserManagementUpdate = () => {
   const updating = useAppSelector(state => state.userManagement.updating);
   const authorities = useAppSelector(state => state.userManagement.authorities);
 
+  useEffect(() => {
+    setUserAuthorities(user.authorities);
+    setUserActivated(user.activated);
+  }, [user.authorities]);
+
   return (
-    <div>
+    <div className="p-2">
       <Row className="justify-content-center">
-        <Col md="8">
+        <Col md="12">
           <h1>
-            <Translate contentKey="userManagement.home.createOrEditLabel">Create or edit a User</Translate>
+            {isNew ? (
+              <Translate contentKey="userManagement.home.createLabel">Create a User</Translate>
+            ) : (
+              <Translate contentKey="userManagement.home.editLabel">Edit a User</Translate>
+            )}
           </h1>
         </Col>
       </Row>
-      <Row className="justify-content-center">
-        <Col md="8">
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <ValidatedForm onSubmit={saveUser} defaultValues={user}>
+
+      <div>
+        {loading ? (
+          <div>
+            <Skeleton className="mb-2"></Skeleton>
+            <Skeleton width="10rem" className="mb-2"></Skeleton>
+            <Skeleton width="5rem" className="mb-2"></Skeleton>
+            <Skeleton height="2rem" className="mb-2"></Skeleton>
+            <Skeleton width="10rem" height="4rem"></Skeleton>
+          </div>
+        ) : (
+          <ValidatedForm onSubmit={saveUser} defaultValues={user}>
+            <Row>
               {user.id ? (
+                <Col md="6">
+                  <ValidatedField
+                    type="text"
+                    name="id"
+                    required
+                    readOnly
+                    label={translate('global.field.id')}
+                    value={user.id}
+                    validate={{ required: true }}
+                  />
+                </Col>
+              ) : null}
+              <Col md="6">
                 <ValidatedField
                   type="text"
-                  name="id"
-                  required
-                  readOnly
-                  label={translate('global.field.id')}
-                  validate={{ required: true }}
+                  name="login"
+                  label={translate('userManagement.login')}
+                  value={user.login}
+                  validate={{
+                    required: {
+                      value: true,
+                      message: translate('register.messages.validate.login.required'),
+                    },
+                    pattern: {
+                      value: /^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$/,
+                      message: translate('register.messages.validate.login.pattern'),
+                    },
+                    minLength: {
+                      value: 1,
+                      message: translate('register.messages.validate.login.minlength'),
+                    },
+                    maxLength: {
+                      value: 50,
+                      message: translate('register.messages.validate.login.maxlength'),
+                    },
+                  }}
                 />
+              </Col>
+              <Col md="6">
+                <ValidatedField
+                  type="text"
+                  name="firstName"
+                  label={translate('userManagement.firstName')}
+                  validate={{
+                    maxLength: {
+                      value: 50,
+                      message: translate('entity.validation.maxlength', { max: 50 }),
+                    },
+                  }}
+                  value={user.firstName}
+                />
+              </Col>
+              <Col md="6">
+                <ValidatedField
+                  type="text"
+                  name="lastName"
+                  label={translate('userManagement.lastName')}
+                  validate={{
+                    maxLength: {
+                      value: 50,
+                      message: translate('entity.validation.maxlength', { max: 50 }),
+                    },
+                  }}
+                  value={user.lastName}
+                />
+                {/* <FormText>This field cannot be longer than 50 characters.</FormText> */}
+              </Col>
+              <Col md="6">
+                <ValidatedField
+                  name="email"
+                  label={translate('global.form.email.label')}
+                  value={user.email}
+                  placeholder={translate('global.form.email.placeholder')}
+                  type="email"
+                  validate={{
+                    required: {
+                      value: true,
+                      message: translate('global.messages.validate.email.required'),
+                    },
+                    minLength: {
+                      value: 5,
+                      message: translate('global.messages.validate.email.minlength'),
+                    },
+                    maxLength: {
+                      value: 254,
+                      message: translate('global.messages.validate.email.maxlength'),
+                    },
+                    validate: v => isEmail(v) || translate('global.messages.validate.email.invalid'),
+                  }}
+                />
+              </Col>
+              <Col md="6">
+                <ValidatedField type="select" name="langKey" label={translate('userManagement.langKey')}>
+                  {locales.map(locale => (
+                    <option value={locale} key={locale}>
+                      {languages[locale].name}
+                    </option>
+                  ))}
+                </ValidatedField>
+              </Col>
+              <Col md="6">
+                <div className="d-flex flex-column gap-2">
+                  <label>{translate('userManagement.profiles')}</label>
+                  <MultiSelect
+                    name="authorities"
+                    options={authorities}
+                    value={userAuthorities}
+                    onChange={e => {
+                      setUserAuthorities(e.value);
+                    }}
+                  />
+                </div>
+                {/* <ValidatedField type="select" name="authorities" multiple label={translate('userManagement.profiles')}>
+                  {authorities.map(role => (
+                    <option value={role} key={role}>
+                      {role}
+                    </option>
+                  ))}
+                </ValidatedField> */}
+              </Col>
+              {user.id ? (
+                <Col md="6">
+                  <Label>{translate('userManagement.activated')}</Label>
+                  <ValidatedField
+                    type="checkbox"
+                    name="activated"
+                    checked={userActivated}
+                    value={userActivated}
+                    onChange={e => setUserActivated(e.target.checked)}
+                  />
+                </Col>
               ) : null}
-              <ValidatedField
-                type="text"
-                name="login"
-                label={translate('userManagement.login')}
-                validate={{
-                  required: {
-                    value: true,
-                    message: translate('register.messages.validate.login.required'),
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$/,
-                    message: translate('register.messages.validate.login.pattern'),
-                  },
-                  minLength: {
-                    value: 1,
-                    message: translate('register.messages.validate.login.minlength'),
-                  },
-                  maxLength: {
-                    value: 50,
-                    message: translate('register.messages.validate.login.maxlength'),
-                  },
-                }}
-              />
-              <ValidatedField
-                type="text"
-                name="firstName"
-                label={translate('userManagement.firstName')}
-                validate={{
-                  maxLength: {
-                    value: 50,
-                    message: translate('entity.validation.maxlength', { max: 50 }),
-                  },
-                }}
-              />
-              <ValidatedField
-                type="text"
-                name="lastName"
-                label={translate('userManagement.lastName')}
-                validate={{
-                  maxLength: {
-                    value: 50,
-                    message: translate('entity.validation.maxlength', { max: 50 }),
-                  },
-                }}
-              />
-              <FormText>This field cannot be longer than 50 characters.</FormText>
-              <ValidatedField
-                name="email"
-                label={translate('global.form.email.label')}
-                placeholder={translate('global.form.email.placeholder')}
-                type="email"
-                validate={{
-                  required: {
-                    value: true,
-                    message: translate('global.messages.validate.email.required'),
-                  },
-                  minLength: {
-                    value: 5,
-                    message: translate('global.messages.validate.email.minlength'),
-                  },
-                  maxLength: {
-                    value: 254,
-                    message: translate('global.messages.validate.email.maxlength'),
-                  },
-                  validate: v => isEmail(v) || translate('global.messages.validate.email.invalid'),
-                }}
-              />
-              <ValidatedField
-                type="checkbox"
-                name="activated"
-                check
-                value={true}
-                disabled={!user.id}
-                label={translate('userManagement.activated')}
-              />
-              <ValidatedField type="select" name="langKey" label={translate('userManagement.langKey')}>
-                {locales.map(locale => (
-                  <option value={locale} key={locale}>
-                    {languages[locale].name}
-                  </option>
-                ))}
-              </ValidatedField>
-              <ValidatedField type="select" name="authorities" multiple label={translate('userManagement.profiles')}>
-                {authorities.map(role => (
-                  <option value={role} key={role}>
-                    {role}
-                  </option>
-                ))}
-              </ValidatedField>
-              <Button tag={Link} to="/admin/user-management" replace color="info">
-                <FontAwesomeIcon icon="arrow-left" />
-                &nbsp;
+            </Row>
+            <div className="d-flex justify-content-end">
+              <Button tag={Link} to="/admin/user-management" replace color="outline-dark">
                 <span className="d-none d-md-inline">
                   <Translate contentKey="entity.action.back">Back</Translate>
                 </span>
               </Button>
               &nbsp;
-              <Button color="primary" type="submit" disabled={isInvalid || updating}>
-                <FontAwesomeIcon icon="save" />
-                &nbsp;
+              <Button color="success" type="submit" disabled={isInvalid || updating}>
                 <Translate contentKey="entity.action.save">Save</Translate>
               </Button>
-            </ValidatedForm>
-          )}
-        </Col>
-      </Row>
+            </div>
+          </ValidatedForm>
+        )}
+      </div>
     </div>
   );
 };
--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -16,8 +16,10 @@ import PageNotFound from 'app/shared/error/page-not-found';
 import { AUTHORITIES } from 'app/config/constants';
 import { sendActivity } from 'app/config/websocket-middleware';
 import Landing from './modules/landing/landing';
+import { AdminSidebar } from './shared/components/admin-sidebar/AdminSidebar';
+import { ProgressSpinner } from 'primereact/progressspinner';
 
-const loading = <div>loading ...</div>;
+const loading = <ProgressSpinner className="d-flex justify-content-center align-items-center" />;
 
 const Account = Loadable({
   loader: () => import(/* webpackChunkName: "account" */ 'app/modules/account'),
