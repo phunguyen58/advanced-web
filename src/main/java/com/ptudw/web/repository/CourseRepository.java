a756666e88a480d42158295352709f7790853756
leapvn=khoanna <anhkhoajjo@gmail.com>
2024-01-05 01:20:49 +0700

    AW-34 | update create class

--- src/main/java/com/ptudw/web/repository/CourseRepository.java
+++ src/main/java/com/ptudw/web/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package com.ptudw.web.repository;
 
 import com.ptudw.web.domain.Course;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface CourseRepository extends JpaRepository<Course, Long>, JpaSpecificationExecutor<Course> {}
+public interface CourseRepository extends JpaRepository<Course, Long>, JpaSpecificationExecutor<Course> {
+    Optional<Course> findOneByCode(String code);
+}
